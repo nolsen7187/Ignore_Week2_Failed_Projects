664f286ec303431596b079a0e2e38433a4080ecb
nolsen7187 <nolsen@regis.edu>
2013-09-09 19:06:28 -0600

    Version 3 of Week 2 Assignment, Added Unit Test Project and main method for compiling reason

---
+++ Week2_FFR_DomainLayer/Week2_FFR_DomainLayer/Execute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_FFR_DomainLayer
+{
+    class Execute
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+
+        }
+    }
+}
